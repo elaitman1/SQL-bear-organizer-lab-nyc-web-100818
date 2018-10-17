8d64623bc295c7e1b1e9fa77c3feaa4d10882835
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-10-17 19:12:18 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,14 +28,12 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(bears.temperment)
-  FROM bears
-  WHERE bears.temperment='goodfy'
+  "SELECT COUNT(bears.temperament) FROM bears WHERE bears.temperament='goofy';"
+
 end
 
 def selects_bear_that_killed_Tim
     "SELECT * FROM bears WHERE bears.name IS NULL;"
-
 end
 #
 # INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
